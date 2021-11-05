da7bf8da2be1cf77dcf5b50f02fd63a8820c8c9c
Anil Pratap Singh <aps.juet@gmail.com>
2021-11-05 17:42:23 +0530

    Application changes

--- train-booking-service/src/main/java/com/train/bookingservice/TrainBookingServiceApplication.java
+++ train-booking-service/src/main/java/com/train/bookingservice/TrainBookingServiceApplication.java
@@ -1,11 +1,14 @@
 package com.train.bookingservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.EnableMBeanExport;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAutoConfiguration
 public class TrainBookingServiceApplication {
 
 	public static void main(String[] args) {
